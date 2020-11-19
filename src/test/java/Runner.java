58e106ab2ad036979ff59a6a1fb11b827ac08651
Oleh Halush <oleh.halush@ctdev.io>
2020-11-19 11:45:16 +0200

    2 tests 1 driver

--- src/test/java/Runner.java
+++ src/test/java/Runner.java
@@ -1,10 +1,37 @@
 public class Runner {
 
-    public static void main(String[] args) {
+    public static void mainOneThread() {
         Tests.beforeClass();
         Tests.beforeTest();
         Tests.openDownloadPage();
         Tests.afterClass();
+
     }
+
+    public static void waitABit() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Tests.beforeClass();
+
+        new Thread(() -> {
+            Tests.beforeTest();
+            Tests.openDownloadPage();
+        }).start();
+
+        new Thread(() -> {
+            Tests.beforeTest();
+            Tests.openFeaturesPage();
+        }).start();
+
+
+//        Tests.afterClass();
+    }
+
 
 }
