7cb6726572ec1029e42eab4c893aee2b059e61d4
Oleh <oleggalushh@gmail.com>
2023-11-09 15:04:31 +0200

    updated web driver manager version and outdated test

--- src/main/java/page/FeaturesPage.java
+++ src/main/java/page/FeaturesPage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class FeaturesPage extends BasePage {
-    @FindBy(xpath = "//div[@class='features_page']//h1[text()='Time tracker']")
+    @FindBy(xpath = "//div[@class='features_page']//h2[text()='Time tracker']")
     private WebElement timeTrackerHeader;
 
     public FeaturesPage(WebDriver driver) {
