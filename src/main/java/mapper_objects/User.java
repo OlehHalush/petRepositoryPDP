1faa970e13041fa32ddfc61239c3088fde0d68be
Oleh Halush <ohalush@n-ix.com>
2023-12-23 22:00:46 +0200

    Removed unused class

--- src/main/java/mapper_objects/User.java
+++ src/main/java/mapper_objects/User.java
@@ -2,9 +2,11 @@ package mapper_objects;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class User {
     private String login;
